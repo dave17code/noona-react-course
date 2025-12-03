b744f7eb49574091b662d40763264941577f7df3
dave17code <dave17code@gmail.com>
2025-12-03 23:29:56 +0900

    weather-app

---
+++ react/weather-app/src/components/WeatherButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const WeatherButton = ({ cities, setCity }) => {
+  console.log("cities?", cities);
+
+  return (
+    <div>
+      <Button variant="warning">Current Location</Button>
+      {cities.map((item, index) => (
+        <Button
+          variant="warning"
+          className="weather-btn"
+          key={index}
+          onClick={() => setCity(item)}
+        >
+          {item}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default WeatherButton;
