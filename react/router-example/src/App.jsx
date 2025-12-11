48f2d42865fc6721afe44d755e185e55c49f0a17
dave17code <dave17code@gmail.com>
2025-12-11 15:33:03 +0900

    react-router-redirect

--- react/router-example/src/App.jsx
+++ react/router-example/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./page/Homepage";
 import Aboutpage from "./page/Aboutpage";
+import Productpage from "./page/Productpage";
+import ProductDetailPage from "./page/ProductDetailPage";
+import LoginPage from "./page/LoginPage";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const PrivateRoute = () => {
+    return isLoggedIn ? <UserPage /> : <Navigate to="/login" />;
+  };
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutpage />} />
+        <Route path="/products" element={<Productpage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </div>
   );
